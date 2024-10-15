60c4f0e1aa22a302a7ef9d6e4174132ecbd8cfa0
RBMedical <rbcheckup@gmail.com>
2024-10-15 13:33:52 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
-const client_Id = '168121174551-apbnhi4ce3fo85u2i74eg24qndo95e7r.apps.googleusercontent.com';
-const client_secret = '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';
-const access_token = 'https://oauth2.googleapis.com/token';
+const client_Id = '168121174551-ij5g6b5l20kjk89n69kk4h7i518vvqrb.apps.googleusercontent.com';
+const client_secret = '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';
+const access_token = 'const access_token ='https://oauth2.googleapis.com/token';
 const apiKey = 'AIzaSyAacPAesETbnRTARngdDeEffxrqBxVmGcg';
 const spreadsheetId = '1_aUWV9uDvVn_WBs25ZsHtVLilUYB9iNP87yadjSbHsw';
 const rangesheet1 = 'data!A2:ZZ'; 
 const rangesheet2 = 'program!A2:ZZ';
 const rangesheet3 = 'register!A2:ZZ';
 const rangesheet4 = 'register!A:A';
-const rangesheet5 = 'sticker!A2:zz';
-const rangesheet6 = 'specimencount!A2:zz';
+const rangesheet5 = 'sticker!A2:ZZ';
+const rangesheet6 = 'specimencount!A2:ZZ';
 
 
 window.onload = function(){
