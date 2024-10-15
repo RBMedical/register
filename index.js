a3c5d30c926e2ec9d34535f57e0957eca7596e08
RBMedical <rbcheckup@gmail.com>
2024-10-15 11:14:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-<script src="api.js"></script>
+const client_Id = '168121174551-apbnhi4ce3fo85u2i74eg24qndo95e7r.apps.googleusercontent.com';
+const client_secret = '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';
+const access_token = 'https://oauth2.googleapis.com/token';
+const apiKey = 'AIzaSyAacPAesETbnRTARngdDeEffxrqBxVmGcg';
+const spreadsheetId = '1_aUWV9uDvVn_WBs25ZsHtVLilUYB9iNP87yadjSbHsw';
+const rangesheet1 = 'data!A2:ZZ'; 
+const rangesheet2 = 'program!A2:ZZ';
+const rangesheet3 = 'register!A2:ZZ';
+const rangesheet4 = 'register!A:A';
+const rangesheet5 = 'sticker!A2:zz';
+const rangesheet6 = 'specimencount!A2:zz';
+
+
 window.onload = function(){
     loadAllRecords();
     displayNextNumber();
