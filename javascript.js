7943eabe1bd6e3a674217996cde222e0488455fa
RBMedical <rbcheckup@gmail.com>
2024-10-17 00:17:45 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,57 @@
-const client_Id = '168121174551-ij5g6b5l20kjk89n69kk4h7i518vvqrb.apps.googleusercontent.com';
-const client_secret = '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';
+window.onload = function() {
+    // Authorization code ที่ได้รับจาก Google OAuth 2.0
+    const code = '4/0AVG7fiRbJb4KRRAMn7ZkpZ8CE5oGcSu5YNsjN_aKYZIyJNPkM50OrJgISzg4hwm40cP-iQ';
+
+    if (code) {
+        // เรียกฟังก์ชันเพื่อแลกเปลี่ยนเป็น access token
+        exchangeCodeForToken(code);
+    } else {
+        console.log("Authorization code not found in URL");
+    }
+};
+
+async function exchangeCodeForToken(code) {
+    const client_id = '168121174551-p6j0heikm2aajscj33ngja68s36t35nr.apps.googleusercontent.com';
+    const client_secret = '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';
+    const redirect_uri = 'https://rbmedical.github.io/register';
+    const tokenEndpoint = 'https://oauth2.googleapis.com/token';
+
+    const params = new URLSearchParams();
+    params.append('code', code);
+    params.append('client_id', client_id);
+    params.append('client_secret', client_secret);
+    params.append('redirect_uri', redirect_uri);
+    params.append('grant_type', 'authorization_code');
+
+    try {
+        const response = await fetch(tokenEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: params.toString()
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log('Access Token:', data.access_token);
+        // ทำการจัดเก็บ access token หรือใช้งานตามความต้องการ
+    } catch (error) {
+        console.error('Error exchanging code for token:', error);
+    }
+}
+
+
+
+
+///const client_Id = '168121174551-ij5g6b5l20kjk89n69kk4h7i518vvqrb.apps.googleusercontent.com';
+///const client_secret = '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';
 const apiKey = 'AIzaSyAacPAesETbnRTARngdDeEffxrqBxVmGcg';
-const redirectUri = 'https://rbmedical.github.io/register/';
-const scope = 'https://www.googleapis.com/auth/spreadsheets';
+///const redirectUri = 'https://rbmedical.github.io/register/';
+///const scope = 'https://www.googleapis.com/auth/spreadsheets';
 const spreadsheetId = '1_aUWV9uDvVn_WBs25ZsHtVLilUYB9iNP87yadjSbHsw';
 const rangesheet1 = 'data!A2:ZZ'; 
 const rangesheet2 = 'program!A2:ZZ';
@@ -10,7 +59,6 @@ const rangesheet3 = 'register!A2:ZZ';
 const rangesheet4 = 'register!A:A';
 const rangesheet5 = 'sticker!A2:ZZ';
 const rangesheet6 = 'specimencount!A2:ZZ';
-const access_token =''; 
 
 
 
