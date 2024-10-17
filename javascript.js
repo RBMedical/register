83ad720a2027c6e438ac39188b854961a1a41e70
RBMedical <rbcheckup@gmail.com>
2024-10-17 07:22:50 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -645,7 +645,7 @@ function sendBarcode(event) {
 
     var barcode = document.getElementById('inputbar').value.trim();
     var barcodeid = barcode.substring(0, 8); // เอา 8 ตัวแรกของบาร์โค้ดมา
-
+ console.log(barcodeid);
     // URL สำหรับ Google Sheets API
     var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${rangesheet3}?key=${apiKey}`;
 
