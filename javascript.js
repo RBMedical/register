11263eacd3efa191b9b5466b960f823ff2fea019
RBMedical <rbcheckup@gmail.com>
2024-10-19 14:10:43 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -650,7 +650,7 @@ function addNewData(access_token) {
         });
     });
    buildSticker();
- closeNewRegister(); // ปิด modal หรือ form ที่เพิ่มข้อมูล
+  // ปิด modal หรือ form ที่เพิ่มข้อมูล
 }
 
 
