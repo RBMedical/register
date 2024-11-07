b9d73e0b9fe644634472280948151c8d195e8649
RBMedical <rbcheckup@gmail.com>
2024-11-07 18:39:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//window.onload = function () {
-//    var newURL = "register/?code=" + encodeURIComponent("4/0AVG7fiSZOkFFZ75UEl-4LnaJqs7lBg-M_JDZUy6jfRPP3BD6vRmizYe-RLFaThS7-bCEow") +       "&scope=" + encodeURIComponent("https://www.googleapis.com/auth/spreadsheets");
+window.onload = function () {
+var newURL = "register/?code=" + encodeURIComponent("4/0AVG7fiRlwIStUI_LVW9yw8eOH90xUIhqnqCeEopkAgVxwYo0G7WxgzTEjlBqJ-durVH73w") +       "&scope=" + encodeURIComponent("https://www.googleapis.com/auth/spreadsheets");
 
-//    window.history.replaceState({ path: newURL }, '', newURL);
-//}
+ window.history.replaceState({ path: newURL }, '', newURL);
+}
 
 
 
@@ -40,8 +40,8 @@ const rangesheet12 = 'specimencount!B1:EE';
 const clientId = "306673689070-g56jn6bs6acs9k9oklrpvh4v3gepfqo6.apps.googleusercontent.com";
 const clientSecret = "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";
 const redirectUri = "https://rbmedical.github.io/register";
-const access_token = 'ya29.a0AeDClZD3_hWob_wqcOKngsIS94MKMsD1yTPjsJW2ciJ0I98S5s7CoGn_s1hPGgi6wSzBr2Ofdn8VdnK2LfmgJ31-BGH58S-k1tJ-sk6luJ4O1uPcCJa6AttZMPAyAPd4JSBXsZILOuwe5juNmD7eujFy5xWoxBDoMjh8y276aCgYKAXQSARESFQHGX2MiGFqA-PAJKZTu3Qjt_UADrg0175';
-const refreshToken = '1//05QlNlg88oXckCgYIARAAGAUSNwF-L9IragF1679dg8axEq5vjUWK3H9m2T5KEXi7S7Ez9xv19FnkHJNI7_EsMrtHYhXyTUNuJkg';
+const access_token = 'ya29.a0AeDClZCbzvO9da7oHdgXoD26k5OSZAJuwtDDky6Ewwznzo7ZpiD4i2qwiiXQDGnUrmFI9iMwGrf5USGNtUCIo37o5knbmVwtD4KtoiNBRzeT8cSNLakeeyrXOC-i0LDQGUy71ZzPXKVKirdidqEaJJI7-fp-e8oFbermt2XHaCgYKAWkSARESFQHGX2MiLXdqptKJp-YWWTbQe5PDmA0175';
+const refreshToken = '1//05G-jdzz-V1kzCgYIARAAGAUSNwF-L9IrqyYoZcHSVZhA3IJfV80KQXDBaBtg53d1zFxgOlUTgW0SuqCVqBA7eTowfrANq3oDIB0';
 const scope = 'https://www.googleapis.com/auth/spreadsheets';
 
 
@@ -1736,13 +1736,13 @@ async function fetchData() {
             });
         });
 
-//window.onload = function () {
-//  loadAllRecords();
-//   getNextNumber(); 
-//   updateDateTime();
-//   loadDataTable();
-//   loadAllCount();
+window.onload = function () {
+  loadAllRecords();
+  getNextNumber(); 
+  updateDateTime();
+  loadDataTable();
+  loadAllCount();
  
- //}
+}
 
 setInterval (checkAndRefreshToken, 2700000);
